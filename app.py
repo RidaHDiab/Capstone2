10beed7225e9685058299eed03da4325db99ee45
DELL <ridadiab7005@gmil.com>
2023-11-20 14:55:31 +0200

    commit 1

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import base64
 import urllib.parse
 
 from PIL import Image
-from flask import Flask, jsonify, redirect, url_for, render_template
+from flask import Flask, jsonify, redirect, url_for, render_template, request
 from flask_cors import CORS
 from pymongo import MongoClient
 from bson import ObjectId
@@ -13,6 +13,7 @@ db = client.news
 
 CORS(app)  # Enable CORS for your Flask app
 
+
 @app.route('/')
 def homepage():
     article = list(db.articles.find({}))
@@ -21,8 +22,16 @@ def homepage():
     return render_template('homepage.html', data=article)
 
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+        user = {
+            'name': request.form.get('name'),
+            'email': request.form.get('email'),
+            'password': request.form.get('password')
+        }
+        db.users.insert_one(user)
+        return redirect('/')
     return render_template('signup.html')
 
 
